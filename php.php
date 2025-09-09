30035bf41f41816c03e98eaa8fd0315bd038b099
dylee616 <dylee@idevel.kr>
2025-09-09 14:23:04 +0900

    1

---
+++ php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Function</title>
+</head>
+
+<body>
+
+    <form action="php.php" method="post" enctype="multipart/form-data">
+            업로드할 파일을 선택하세요.<br>
+            <input type="file" name="upload"><br><br>
+            <input type="submit" value="업로드"
+    </form>
+
+</body>
+
+</html>
